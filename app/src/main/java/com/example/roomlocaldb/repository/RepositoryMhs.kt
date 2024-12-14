35c1a146a5ac202ae894f58474e9596186f4f81f
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-14 18:22:04 +0700

    Menambahkan fungsi getAllMhs untuk mendapatkan data semua mahasiswa

--- app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
@@ -1,7 +1,12 @@
 package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs () : Flow<List<Mahasiswa>>
+
 }
+
