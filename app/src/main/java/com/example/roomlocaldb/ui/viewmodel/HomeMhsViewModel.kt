578aceae280ae03ee47190f6fa9ca489a9b024c5
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-15 16:07:25 +0700

    membuat class HomeViewModel untuk memanfaatkan Flow dari repository untuk mengelola state UI

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.roomlocaldb.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import com.example.roomlocaldb.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+):ViewModel(){
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+}
 
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
