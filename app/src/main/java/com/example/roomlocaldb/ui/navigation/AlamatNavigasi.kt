7db8d7633e2d16e4cdb7ca738eee653a6b0219dc
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-15 15:43:35 +0700

    menambahkan objek navigasi untuk mengatur alamat atau rute navigasi

--- app/src/main/java/com/example/roomlocaldb/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/navigation/AlamatNavigasi.kt
@@ -2,4 +2,19 @@ package com.example.roomlocaldb.ui.navigation
 
 interface AlamatNavigasi {
     val route: String
+
+    object DestinasiHome : AlamatNavigasi{
+        override val route  = "Home"
+    }
+
+    object DestinasiDetail : AlamatNavigasi{
+        override val route = "detail"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM"
+    }
+    object DestinasiUpdate : AlamatNavigasi{
+        override val route = "update"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM"
+    }
 }
