7a519520e3a614601ecf48d5ae3ed94a3e103897
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-15 17:21:03 +0700

    membuat class UpdateMhsViewModel lalu menambahkan fungsi updateState, ValidasiFields didalamnya

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,9 +1,63 @@
 package com.example.roomlocaldb.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewModelScope
 import com.example.roomlocaldb.KrsApp
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import com.example.roomlocaldb.repository.RepositoryMhs
+import com.example.roomlocaldb.ui.navigation.AlamatNavigasi
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+
+    var updateUiState by mutableStateOf(MhsUiState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiEdit.NIM])
+
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUiStateMhs()
+        }
+    }
+
+    fun updateState (mahasiswaEvent: MahasiswaEvent) {
+        updateUiState = updateUiState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
+
+    fun validateFields() : Boolean {
+        val event = updateUiState.mahasiswaEvent
+        val  errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jeniskelamin = if(event.jeniskelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if(event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if(event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if(event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong",
+        )
+
+        updateUiState = updateUiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+
+}
 
 fun Mahasiswa.toUiStateMhs () : MhsUiState = MhsUiState(
     mahasiswaEvent = this.toDetailUiEvent(),
