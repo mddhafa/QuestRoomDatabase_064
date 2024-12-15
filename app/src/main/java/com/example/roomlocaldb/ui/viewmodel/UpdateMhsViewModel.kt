fb42696d64bc16cd679dd704fb420bf0c8476aa2
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-15 17:03:59 +0700

    membuat initializer untuk penyedia view model

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,8 +1,30 @@
 package com.example.roomlocaldb.ui.viewmodel
 
+import androidx.lifecycle.createSavedStateHandle
+import com.example.roomlocaldb.KrsApp
 import com.example.roomlocaldb.data.entity.Mahasiswa
 
 
 fun Mahasiswa.toUiStateMhs () : MhsUiState = MhsUiState(
     mahasiswaEvent = this.toDetailUiEvent(),
 )
+
+initializer {
+    HomeMhsViewModel(
+        KrsApp() .containerApp.repositoryMhs,
+    )
+}
+
+initializer {
+    DetailMhsViewModel(
+        createSavedStateHandle(),
+        KrsApp().containerApp.repositoryMhs,
+    )
+}
+
+initializer {
+    UpdateMhsViewModel (
+        createSavedStateHandle(),
+        KrsApp().containerApp.repositoryMhs,
+    )
+}
