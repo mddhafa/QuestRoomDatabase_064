ba8c1cba6ca68e60a000729431bf37bb00d3d9e4
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-15 15:31:29 +0700

    menambahkan method untuk memanggil fungsi allmahasiswa dari mahasiswa dao untuk menfapatkan semua data mahasiswa

--- app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,18 @@ package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.dao.MahasiswDao
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswDao: MahasiswDao
 ) : RepositoryMhs{
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        mahasiswDao.inseetMahasiswa(mahasiswa)
+        mahasiswDao.insertMahasiswa(mahasiswa)
     }
+
+   override fun getAllMhs () : Flow<List<Mahasiswa>>{
+        return mahasiswDao.getAllMahasiswa()
+   }
 }
+
+
