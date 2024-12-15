47433bc241c6af88ca8dc2790a65467764a8c189
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-15 16:24:38 +0700

    membuat fungsi mahasiswa

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.roomlocaldb.ui.viewmodel
 
+import com.example.roomlocaldb.data.entity.Mahasiswa
+
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
@@ -11,4 +13,16 @@ data class DetailUiState(
 
     val isEventNotEmpty: Boolean
         get () = detailUiEvent != MahasiswaEvent()
+}
+
+//memindahkan data dari entity ke ui
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jeniskelamin = jeniskelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
