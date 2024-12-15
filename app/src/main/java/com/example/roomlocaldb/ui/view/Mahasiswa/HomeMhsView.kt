f09fc53a16262b6624b3cba3c35b98249f136e61
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-15 18:38:33 +0700

    menambahkan row di dalam card

--- app/src/main/java/com/example/roomlocaldb/ui/view/Mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/view/Mahasiswa/HomeMhsView.kt
@@ -1,14 +1,29 @@
 package com.example.roomlocaldb.ui.view.Mahasiswa
 
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import org.w3c.dom.Text
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,10 +38,44 @@ fun CardMhs(
             .fillMaxWidth()
             .padding(8.dp)
     ){
-        Row (
-
+        Column(
+            modifier= Modifier.padding(8.dp)
         ) {
-
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.nim,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.kelas,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
         }
     }
 }
